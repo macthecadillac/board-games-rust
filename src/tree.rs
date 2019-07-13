47d364392fbe9da4a46b1174b495753942718cc2
Mac Lee <macthecadillac@gmail.com>
2019-07-13 14:45:18 -0700

    More usability changes

--- src/tree.rs
+++ src/tree.rs
@@ -1,54 +1,84 @@
 /// A tree structure that does not allow adding siblings but optimizes serial
 /// children access
-use std::ops::Range;
+use std::ops::{Index, IndexMut, Range};
+use crate::mcts::{NodeType, ReturnType};
+
+#[derive(Copy, Clone)]
+pub struct NodeIndx(usize);
 
 #[derive(Clone)]
-pub struct Cell<T> where T: Clone {
-    indx: usize,
-    data: T,
+pub struct Node<T> where T: Clone {
+    pub data: T,
+    indx: Option<NodeIndx>,
     children: Option<Range<usize>>
-}
-
-impl<T> Cell<T> where T: Clone {
-    fn to_node(self) -> Node<T> {
-        match self.children {
-            None => Node::Leaf(self),
-            Some(_) => Node::Node(self)
-        }
-    }
 }
 
 pub struct Tree<T> where T: Clone {
     arena: Vec<Node<T>>
 }
 
-pub enum Node<T> where T: Clone {
-    Node(Cell<T>),
-    Leaf(Cell<T>)
-}
-
 impl<'a, T> Node<T> where T: Clone {
     pub fn children(&'a self, tree: &'a mut Tree<T>) -> &mut [Node<T>] {
-        match self {
-            Node::Leaf(_) => &mut [],
-            Node::Node(cell) => &mut tree.arena[cell.children.clone().unwrap()]
+        match self.children.clone() {
+            None => &mut [],
+            Some(range) => &mut tree.arena[range.clone()]
         }
     }
 
-    pub fn append(self, tree: &mut Tree<T>, mut children: Vec<Cell<T>>) {
-        match self {
-            Node::Node(_) => panic!("Can't add to already populated node"),
-            Node::Leaf(mut cell) => {
+    pub fn append(&mut self, tree: &mut Tree<T>, mut children: Vec<Node<T>>) {
+        match self.children {
+            Some(_) => panic!("Can't add to an already populated node"),
+            None => {
                 let range_start = tree.arena.len();
-                let mut children: Vec<_> = children.iter_mut()
-                    .map(|x| x.clone().to_node())
-                    .collect();
                 tree.arena.append(&mut children);
                 let range_end = tree.arena.len();
-                cell.children = Some(range_start..range_end);
-                let indx = cell.indx;
-                tree.arena[indx] = Node::Node(cell);
+                self.children = Some(range_start..range_end);
             }
         }
+    }
+
+    pub fn index(&self) -> Option<NodeIndx> {
+        self.indx
+    }
+}
+
+impl<T> From<T> for Node<T> where T: Clone{
+    fn from(item: T) -> Self { Node { data: item, indx: None, children: None } }
+}
+
+impl<T> ReturnType<T> for Node<T> where T: Clone {
+    fn node_type(&self, tree: &Tree<T>) -> NodeType {
+        match self.children {
+            Some(_) => NodeType::Node,
+            None => NodeType::Leaf
+        }
+    }
+}
+
+impl<'a, T> Tree<T> where T: Clone {
+    pub fn new() -> Self { Tree { arena: Vec::new() } }
+
+    pub fn add_node(&'a mut self, node_data: T) -> &mut Node<T> {
+        let indx = Some(NodeIndx(self.arena.len()));
+        self.arena.push(Node { data: node_data, indx, children: None });
+
+        let len = self.arena.len();
+        &mut self.arena[len]
+    }
+}
+
+impl<T> Index<NodeIndx> for Tree<T> where T: Clone {
+    type Output = Node<T>;
+
+    fn index(&self, index: NodeIndx) -> &Self::Output {
+        let NodeIndx(i) = index;
+        &self.arena[i]
+    }
+}
+
+impl<T> IndexMut<NodeIndx> for Tree<T> where T: Clone {
+    fn index_mut(&mut self, index: NodeIndx) -> &mut Self::Output {
+        let NodeIndx(i) = index;
+        &mut self.arena[i]
     }
 }
